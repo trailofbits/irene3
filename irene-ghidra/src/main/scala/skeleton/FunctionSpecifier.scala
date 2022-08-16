0962725f7612a1497456409aa5da03f041778986
2over12 <ian.smith@trailofbits.com>
2022-08-16 15:21:58 -0400

    bump to scala3

---
+++ irene-ghidra/src/main/scala/skeleton/FunctionSpecifier.scala
@@ -0,0 +1,11 @@
+package skeleton
+
+import ghidra.program.model.listing.Function;
+
+object FunctionSpecifier {
+
+  def specifyFunction(func: Function) = {
+    if 1 < 0 then println("")
+    else println("")
+  }
+}
