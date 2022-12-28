92b5cfde845625df31c56a0617d5953ef32d62ce
2over12 <ian.smith@trailofbits.com>
2022-12-27 21:47:40 -0500

    still need to implement widening

--- irene-ghidra/src/main/scala/anvill/CompositionalValueAnalysis.scala
+++ irene-ghidra/src/main/scala/anvill/CompositionalValueAnalysis.scala
@@ -1,33 +1,76 @@
 package anvill
 
-enum Space:
-  case Uniq, Reg, Ram
+import ghidra.program.model.block.CodeBlock
 
-case class ALoc(val spc: Space, val offset: Int, val byte_sz: Int)
+import ghidra.program.model.listing
+
+import ghidra.program.model.pcode.PcodeOp
+import ghidra.program.model.listing.Variable
+import scala.collection.immutable.TreeMap
+import ghidra.program.model.pcode.Varnode
+
+sealed abstract class ALoc
+case class Reg(val reg: ghidra.program.model.lang.Register) extends ALoc
+case class StackVar(val offset: Int) extends ALoc
 
 sealed abstract class BaseVal
 
-object Empty extends BaseVal
 case class Parameter(val ind: Int) extends BaseVal
-case class Register(val name: String) extends BaseVal
+case class Register(val name: ghidra.program.model.lang.Register)
+    extends BaseVal
 
 sealed abstract class DomainValue
-case class DomainValue(
+case class RegOff(
     val base: BaseVal,
     val offset: SignAgnosticStridedInterval
-)
+) extends DomainValue
+case class Global(val offset: SignAgnosticStridedInterval) extends DomainValue
+object DomValBot extends DomainValue
+// For tracking non local addresses so that we do not need to blow away local A-Locs
+object GlobalConst extends DomainValue
+object DomValTop extends DomainValue
 
 object DomainValue extends JoinSemiLatice[DomainValue] {
 
-  override def tryCompare(x: DomainValue, y: DomainValue): Option[Int] = ???
+  override def tryCompare(x: DomainValue, y: DomainValue): Option[Int] = {
+    if lteq(x, y) && lteq(y, x) then Some(0)
+    else if lteq(x, y) then Some(1)
+    else if lteq(y, x) then Some(-1)
+    else None
+  }
 
-  override def lteq(x: DomainValue, y: DomainValue): Boolean = ???
+  override def lteq(x: DomainValue, y: DomainValue): Boolean = {
+    (x, y) match {
+      case (DomValTop, DomValTop)   => true
+      case (DomValTop, _)           => false
+      case (DomValBot, _)           => true
+      case (Global(_), GlobalConst) => true
+      case (Global(x), Global(y))   => SignAgnosticStridedInterval.lteq(x, y)
+      case (RegOff(x_base, x_off), RegOff(y_base, y_off)) if x_base == y_base =>
+        SignAgnosticStridedInterval.lteq(x_off, y_off)
+      case (_, _) => false
+    }
+  }
 
-  override def join(lhs: DomainValue, rhs: DomainValue): DomainValue = ???
+  override def join(lhs: DomainValue, rhs: DomainValue): DomainValue =
+    (lhs, rhs) match {
+      case (DomValBot, x) => x
+      case (x, DomValBot) => x
+      case (DomValTop, _) => DomValTop
+      case (_, DomValTop) => DomValTop
+      case (Global(x), Global(y)) =>
+        Global(SignAgnosticStridedInterval.join(x, y))
+      case (Global(_), GlobalConst)   => GlobalConst
+      case (GlobalConst, Global(_))   => GlobalConst
+      case (GlobalConst, GlobalConst) => GlobalConst
+      case (_, _)                     => DomValTop
+    }
 
-  override val bot: DomainValue = ???
+  override val bot: DomainValue = DomValBot
 
 }
+
+type DomType = Map[ALoc, DomainValue]
 
 object Dom extends MappingDomain[ALoc, DomainValue](DomainValue)
 
@@ -71,4 +114,114 @@ class MappingDomain[K, V](po: JoinSemiLatice[V])
   *
   * The domain of analysis is a mapping domain from a-loc -> symm + ESI
   */
-class CompositionalValueAnalysis {}
+class CompositionalValueAnalysis(
+    val function: ghidra.program.model.listing.Function
+) extends PcodeOpTransferFunction[DomType] {
+
+  val stack_offset_to_size: TreeMap[Int, Int] =
+    (TreeMap() ++
+      function
+        .getAllVariables()
+        .filter(v => v.isStackVariable())
+        .map(v => (v.getStackOffset(), v.getLength()))
+        .toSeq)
+
+  def generate_value(vstorage: Variable): DomainValue = {
+    val zero_vec = BitVector(BigInt(0), vstorage.getLength())
+    val zero_sasi = SASIValue(BigInt(1), zero_vec, zero_vec)
+    if (vstorage.isInstanceOf[ghidra.program.model.listing.Parameter]) {
+      RegOff(
+        Parameter(
+          vstorage
+            .asInstanceOf[ghidra.program.model.listing.Parameter]
+            .getOrdinal()
+        ),
+        zero_sasi
+      )
+    } else if (vstorage.isRegisterVariable()) {
+      RegOff(Register(vstorage.getRegister()), zero_sasi)
+    } else {
+      DomValBot
+    }
+  }
+
+  def generate_aloc(vstorage: Variable): Option[ALoc] = {
+    if vstorage.getVariableStorage().isCompoundStorage() then None
+    else if vstorage.isStackVariable() then
+      Some(
+        StackVar(vstorage.getStackOffset())
+      )
+    else if vstorage.isRegisterVariable() then {
+      val reg = vstorage.getRegister().getParentRegister()
+
+      Some(
+        Reg(reg)
+      )
+    } else None
+  }
+
+  def set_initial_values(
+      func: ghidra.program.model.listing.Function,
+      prev: DomType
+  ): DomType = {
+    func
+      .getAllVariables()
+      .foldLeft(prev)((curr, param) =>
+        (for {
+          aloc <- generate_aloc(param)
+        } yield (aloc, generate_value(param)))
+          .map((k, v) => curr.updated(k, v))
+          .getOrElse(curr)
+      )
+  }
+
+  def execute_block_entrance(
+      func: ghidra.program.model.listing.Function,
+      curr_blk: CodeBlock,
+      prev: DomType
+  ): DomType = {
+    if (curr_blk.getFirstStartAddress() == func.getEntryPoint()) {
+      set_initial_values(func, prev)
+    } else {
+      prev
+    }
+  }
+
+  // the only static aloc that is not a computed pointer
+  // for now are registers since we dont track memory
+  def find_aloc(vnode: Varnode): Option[ALoc] = {
+    Option(
+      function
+        .getProgram()
+        .getLanguage()
+        .getRegister(vnode.getAddress(), vnode.getSize())
+    ).map(r => Reg(r.getParentRegister()))
+  }
+
+  def assign_value(
+      output_vnode: Varnode,
+      value: DomainValue,
+      prev_dom: DomType
+  ): DomType = {
+    find_aloc(output_vnode)
+      .map(aloc => prev_dom.updated(aloc, value))
+      .getOrElse(prev_dom)
+  }
+
+  override def execute_pcode(
+      f: listing.Function,
+      pc: PcodeOp,
+      curr: DomType
+  ): DomType = {
+    ???
+  }
+
+  override def step(
+      f: listing.Function,
+      i: Int,
+      updating_blk: CodeBlock,
+      prev: DomType,
+      next: DomType
+  ): DomType = ???
+
+}
