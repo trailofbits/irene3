8ef963b790afe66bd0552f034d936b14a4397318
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2023-01-24 16:21:46 +0100

    Begin work on low-level stack

--- bin/Codegen/Main.cpp
+++ bin/Codegen/Main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char *argv[]) {
 
     irene3::TypeDecoder type_decoder;
     auto maybe_spec = irene3::ProtobufPathToDecompilationBuilder(
-        FLAGS_spec, FLAGS_type_propagation, /*args_as_locals=*/false, type_decoder);
+        FLAGS_spec, FLAGS_type_propagation, /*args_as_locals=*/true, type_decoder);
     if (!maybe_spec.Succeeded()) {
         std::cerr << maybe_spec.TakeError() << std::endl;
         return EXIT_FAILURE;
