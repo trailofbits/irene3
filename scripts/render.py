3a3c7d51023970dd1bf38f52e92d4bbac2891ea1
William Tan <1284324+Ninja3047@users.noreply.github.com>
2023-02-10 10:50:34 -0500

    fix render script output

--- scripts/render.py
+++ scripts/render.py
@@ -1,28 +1,28 @@
 import graphviz
 import json
 import argparse
+import sys
 
 
 def main():
     prsr = argparse.ArgumentParser()
-    prsr.add_argument("target_json")
-    prsr.add_argument("out_dot")
+    prsr.add_argument("target_json", type=argparse.FileType('r'))
+    prsr.add_argument("out_dot", nargs="?", type=argparse.FileType('w'), default=sys.stdout)
 
     args = prsr.parse_args()
 
-    with open(args.target_json, "r") as f:
-        patch_specs = json.load(f)
+    patch_specs = json.load(args.target_json)
 
     digraph = graphviz.Digraph("Patches")
 
     for nd in patch_specs["patches"]:
         digraph.node(nd["patch-addr"],
-                     nd["patch-code"].replace("\n", "\l"), shape="rectangle")
+                     nd["patch-code"].replace("\n", r"\l"), shape="rectangle")
 
         for e in nd["edges"]:
             digraph.edge(nd["patch-addr"], e)
 
-    print(digraph)
+    args.out_dot.write(str(digraph))
 
 
 if __name__ == "__main__":
