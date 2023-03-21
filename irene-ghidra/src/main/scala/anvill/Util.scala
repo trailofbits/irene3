7c47b0ad2c1c5236c4cd3128d702d0767c52736b
2over12 <ian.smith@trailofbits.com>
2023-03-21 15:40:58 -0400

    share permissive block discovery between liveness and specification

--- irene-ghidra/src/main/scala/anvill/Util.scala
+++ irene-ghidra/src/main/scala/anvill/Util.scala
@@ -23,6 +23,16 @@ import ghidra.program.model.lang.Register
 import ProgramSpecifier.getRegisterName
 import specification.specification.TypeSpec.Type
 import specification.specification.BaseType.BT_U8
+import ghidra.util.Msg
+import scala.collection.mutable.Buffer
+import ghidra.util.task.TimeoutTaskMonitor
+import java.util.concurrent.TimeUnit
+import java.util.Objects
+import ghidra.program.model.listing.Program
+import ghidra.program.model.listing.Instruction
+import specification.specification.{CodeBlock => CodeBlockSpec}
+import anvill.ProgramSpecifier.specifyContextAssignments
+
 object Util {
 
   def sizeToArray(sz: Int): TypeSpec = {
@@ -71,7 +81,10 @@ object Util {
   def blkToEdges(func: Function, blk: CodeBlock): Seq[DiEdge[CodeBlock]] = {
     val child_blks =
       collectRefs(blk.getDestinations(TaskMonitor.DUMMY)).filter(ref =>
-        func.getBody().contains(ref.getDestinationAddress())
+        isFuncAddr(func, ref.getDestinationAddress()) && isValidBlock(
+          func.getProgram(),
+          ref.getDestinationBlock()
+        )
       )
     child_blks.map(c => DiEdge((blk, c.getDestinationBlock())))
   }
@@ -86,14 +99,102 @@ object Util {
       )
       .toSet
   }
+
+  def isFuncAddr(func: Function, addr: Address): Boolean = {
+    val maybe_func = Option(
+      func
+        .getProgram()
+        .getFunctionManager()
+        .getFunctionContaining(addr)
+    )
+
+    !addr.isExternalAddress() && maybe_func
+      .map(_ == func)
+      .getOrElse(true)
+  }
+
+  def isFuncBlock(func: Function, blk: CodeBlock): Boolean = {
+    isFuncAddr(func, blk.getFirstStartAddress())
+  }
+
+  // We depend on some assumptions about basic blocks
+  // for now we rely on blocks being contigous non empty sequences of instructions
+  def isValidBlock(program: Program, blk: CodeBlock): Boolean = {
+    val blkinsns: ju.Iterator[Instruction] = program
+      .getListing()
+      .getInstructions(blk, true)
+    val blkinsnsseq = blkinsns.asScala.toSeq
+
+    !blkinsnsseq.isEmpty && blkinsnsseq.map(_.getLength()).sum.toLong == ((blk
+      .getMaxAddress()
+      .getOffset() - blk.getFirstStartAddress().getOffset()) + 1)
+  }
+
+  def getValidAddresses(
+      func: Function,
+      it: CodeBlockReferenceIterator,
+      addr: CodeBlockReference => Address
+  ): Seq[Address] = {
+    collectRefs(it)
+      .map(addr(_))
+      .filter(Util.isFuncAddr(func, _))
+  }
+
+  def getReachableCodeBlocks(func: Function): Seq[CodeBlock] = {
+    val res: Buffer[CodeBlock] = Buffer()
+    val closed_list: scala.collection.mutable.Set[Long] =
+      scala.collection.mutable.Set()
+    val prog = func.getProgram()
+    val listing = prog.getListing()
+    val model = BasicBlockModel(prog)
+    val queue = scala.collection.mutable.Queue[Address]()
+    val monitor = TaskMonitor.DUMMY
+
+    queue.enqueue(func.getEntryPoint())
+    while (queue.size > 0) {
+      val addr = queue.dequeue()
+
+      if (!closed_list.contains(addr.getOffset())) {
+        closed_list.add(addr.getOffset())
+        val block = model.getCodeBlockAt(addr, monitor)
+
+        if (
+          Objects.nonNull(
+            block
+          ) && isValidBlock(func.getProgram(), block)
+        ) {
+          // If we arent going to consider this block then we may as well not consider its successors unless we encounter them somehow
+          // on a different path
+          queue ++= getValidAddresses(
+            func,
+            block.getSources(monitor),
+            ref => ref.getSourceAddress()
+          )
+
+          queue ++= getValidAddresses(
+            func,
+            block.getDestinations(monitor),
+            ref => ref.getDestinationAddress()
+          )
+
+          res += block
+
+        } else {
+          Msg.warn(this, s"Skipping invalid block: $addr")
+        }
+      }
+    }
+    res.toSeq
+  }
+
   def getCfgAsGraph(func: Function): CFG = {
     val model = BasicBlockModel(func.getProgram)
-    val blks = model.getCodeBlocksContaining(func.getBody(), TaskMonitor.DUMMY)
+    val blks = getReachableCodeBlocks(func).iterator
 
     val edge_buff: ListBuffer[DiEdge[CodeBlock]] = ListBuffer()
     val nodes: ListBuffer[CodeBlock] = ListBuffer()
 
-    while (blks.hasNext()) {
+    while (blks.hasNext) {
       val curr = blks.next()
       edge_buff.addAll(blkToEdges(func, curr))
       nodes += curr
