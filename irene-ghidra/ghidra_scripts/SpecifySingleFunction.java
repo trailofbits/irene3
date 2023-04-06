03adc5b6ac2406373addda3ef1de6f32561a7630
Alex Cameron <asc@tetsuo.sh>
2023-04-07 09:35:03 +1000

    Allow `SpecifySingleFunction` script to emit JSON (#191)
    * Allow `SpecifySingleFunction` script to emit JSON
    
    * Make format choice strings `private`

--- irene-ghidra/ghidra_scripts/SpecifySingleFunction.java
+++ irene-ghidra/ghidra_scripts/SpecifySingleFunction.java
@@ -1,9 +1,9 @@
 //TODO write a description for this script
-//@author 
+//@author
 //@category _NEW_
-//@keybinding 
-//@menupath 
-//@toolbar 
+//@keybinding
+//@menupath
+//@toolbar
 
 import ghidra.app.script.GhidraScript;
 import ghidra.program.model.mem.*;
@@ -17,19 +17,37 @@ import ghidra.program.model.symbol.*;
 import ghidra.program.model.scalar.*;
 import ghidra.program.model.listing.*;
 import ghidra.program.model.address.*;
+import scalapb.json4s.JsonFormat;
 import anvill.ProgramSpecifier;
 
+import java.util.Arrays;
 import java.io.FileOutputStream;
 
 public class SpecifySingleFunction extends GhidraScript {
 
+    private String PROTOBUF_CHOICE = "Protobuf";
+    private String JSON_CHOICE = "JSON";
+
     public void run() throws Exception {
 	var func =  this.currentProgram.getFunctionManager().getFunctionContaining(currentLocation.getAddress());
 	var spec = ProgramSpecifier.specifySingleFunction(func);
-	
+
+        var format = askChoice("Choose output format", "Choose an output format to export as",
+            Arrays.asList(PROTOBUF_CHOICE, JSON_CHOICE), PROTOBUF_CHOICE);
+        if (format == null) {
+            return;
+        }
+
 	var file = askFile("Create output file" ,"Create");
         var strm = new FileOutputStream(file);
-        spec.writeTo(strm);
+
+        if (format.equals(PROTOBUF_CHOICE)) {
+            spec.writeTo(strm);
+        } else {
+            assert(format.equals(JSON_CHOICE));
+            var json = JsonFormat.toJsonString(spec);
+            strm.write(json.getBytes());
+        }
     }
 
 }
