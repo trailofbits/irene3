f9cbccd6a7095a041e872fdc5c6ba7943aabeee6
William Tan <1284324+Ninja3047@users.noreply.github.com>
2024-01-12 15:10:23 -0500

    Improve patchlang build (#360)
    use submodule instead of separate clone
    bump cxx-common, newest cxx-common includes gap dependencies and re2c
    add docker tests for each tool for challenge 3 patch workflow
    fix docker container for arm64 linux
    add patch-assembler as a script to pyproject.toml
    add doc with some instructions for challenge 3 patchlang workflow

---
+++ scripts/download_and_unpack_demo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+mkdir -p ./tests/demo
+cd ./tests/demo || exit 1
+if [[ -z "$(ls -A ./)" ]]
+then
+    if [[ -z "$TOB_AMP_PASSPHRASE" ]]
+    then
+        echo "Please provide a passhphrase in TOB_AMP_PASSPHRASE"
+        exit 1
+    fi
+    curl -LO http://tob-amp-share.nyc3.digitaloceanspaces.com/demo.tar.gz.gpg
+    gpg --no-tty --batch --pinentry-mode loopback --passphrase "$TOB_AMP_PASSPHRASE" -o demo.tar.gz --decrypt demo.tar.gz.gpg
+    tar --strip-components=1 -xvf ./demo.tar.gz
+fi
