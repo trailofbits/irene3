2a4ed1e14be62f168225773249ef1fca5334ff60
2over12 <ian.smith@trailofbits.com>
2024-03-01 19:21:26 -0500

    Fix demo ci (#410)
    * fix ci
    
    * fix more paths
    
    * another one

--- scripts/download_and_unpack_demo.sh
+++ scripts/download_and_unpack_demo.sh
@@ -10,5 +10,5 @@ then
     fi
     curl -LO http://tob-amp-share.nyc3.digitaloceanspaces.com/demo.tar.gz.gpg
     gpg --no-tty --batch --pinentry-mode loopback --passphrase "$TOB_AMP_PASSPHRASE" -o demo.tar.gz --decrypt demo.tar.gz.gpg
-    tar --strip-components=1 -xvf ./demo.tar.gz
+    tar --strip-components=2 -xvf ./demo.tar.gz
 fi
